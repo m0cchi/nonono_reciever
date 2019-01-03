18cd003d31a22539d81323ff4206a950449a437a
mocchi <boom.boom.planet@gmail.com>
2019-01-03 18:11:08 +0900

    initial commit

---
+++ lib/nonono_reciever.rb
@@ -0,0 +1,6 @@
+require "nonono_reciever/version"
+
+module NononoReciever
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/nonono_reciever/version.rb
@@ -0,0 +1,3 @@
+module NononoReciever
+  VERSION = "0.1.0"
+end
